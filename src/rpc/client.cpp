3183018bce9400e00f42aca17e06c4d8a972a3e2
CryptAxe <cryptaxe@gmail.com>
2017-05-21 22:06:34 -0700

    Add SCDB and BMM RPC calls
    * receivesidechainwtjoin
    
    * listsidechaindeposits
    
    * getbmmproof
    
    * createbribe
    
    * refundbribe

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -130,6 +130,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "logging", 1, "exclude" },
     { "disconnectnode", 1, "nodeid" },
     { "addwitnessaddress", 1, "p2sh" },
+    { "refundbribe", 2, "pos" },
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
