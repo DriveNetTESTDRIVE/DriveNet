ed29c823728f310bfb0054a54c8c4dc3e6518c02
CryptAxe <cryptaxe@gmail.com>
2017-09-06 09:13:57 -0700

    Fix logic error and typo

--- src/validation.cpp
+++ src/validation.cpp
@@ -2897,7 +2897,7 @@ CScript GenerateSCDBCoinbaseCommitment()
     return script;
 }
 
-CScript GeneratBMMCriticalHashCommitment()
+CScript GenerateBMMCriticalHashCommitment()
 {
     // TODO
     // check consensusParams.vDeployments[Consensus::DEPLOYMENT_DRIVECHAINS]
