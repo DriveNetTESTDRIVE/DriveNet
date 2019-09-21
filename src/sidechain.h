c776396e351bd3c6a2864dd7693a42c5049b92ac
CryptAxe <cryptaxe@gmail.com>
2019-09-21 01:24:43 -0700

    Shorten WT^ verification period and workscore requirement

--- src/sidechain.h
+++ src/sidechain.h
@@ -11,8 +11,8 @@
 #include <array>
 
 // These are the temporary values to speed things up during testing
-static const int SIDECHAIN_VERIFICATION_PERIOD = 300;
-static const int SIDECHAIN_MIN_WORKSCORE = 141;
+static const int SIDECHAIN_VERIFICATION_PERIOD = 100;
+static const int SIDECHAIN_MIN_WORKSCORE = 50;
 
 // These are the values that will be used in the final release
 //static const int SIDECHAIN_VERIFICATION_PERIOD = 26298;
