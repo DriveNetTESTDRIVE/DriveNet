a72acc41bd5d09141aea1b5f769d739923a65fab
CryptAxe <cryptaxe@gmail.com>
2019-10-17 17:06:43 -0700

    Make verification period even shorter

--- src/sidechain.h
+++ src/sidechain.h
@@ -11,8 +11,8 @@
 #include <array>
 
 // These are the temporary values to speed things up during testing
-static const int SIDECHAIN_VERIFICATION_PERIOD = 100;
-static const int SIDECHAIN_MIN_WORKSCORE = 50;
+static const int SIDECHAIN_VERIFICATION_PERIOD = 20;
+static const int SIDECHAIN_MIN_WORKSCORE = 5;
 
 // These are the values that will be used in the final release
 //static const int SIDECHAIN_VERIFICATION_PERIOD = 26298;
